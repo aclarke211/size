877242154e743653f9485e0539d1823a871b0084
Alex Clarke <Alex.Clarke@jdplc.com>
2018-12-20 10:23:37 +0000

    Created Card component.

---
+++ src/components/Card.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hello">
+    <h1>THIS IS A CARD</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card"
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Card/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Card from "@/components/Card.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld,
-  },
+    Card
+  }
 };
 </script>
