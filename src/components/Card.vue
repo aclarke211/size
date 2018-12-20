87391646f07689868474cf89ac22c241649f4a0a
Alex Clarke <Alex.Clarke@jdplc.com>
2018-12-20 10:56:05 +0000

    Title text on Card.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="card__container">
     <ImageModule />
+    <p class="card-title">{{ cardTitle }}</p>
   </div>
 </template>
 
@@ -9,8 +10,28 @@ import ImageModule from '@/modules/ImageModule.vue';
 
 export default {
   name: 'Card',
+  data: () => ({
+    cardTitle: 'SHOE NAME',
+  }),
   components: {
     ImageModule,
-  }
+  },
 };
 </script>
+
+<style lang="scss">
+  .card__container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    .card-title {
+      font-size: 1.25rem;
+      padding: 1.25rem 0;
+      margin: 0;
+      display: flex;
+      justify-content: center;
+      text-align: center;
+    }
+  }
+</style>
