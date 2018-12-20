e61e59d3c32ea67d22c36170c0a497042e80931b
Alex Clarke <Alex.Clarke@jdplc.com>
2018-12-20 10:49:24 +0000

    Created Image Module (+ uses aspect ratio padding).

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="hello">
-    <h1>THIS IS A CARD</h1>
+  <div class="card__container">
+    <ImageModule />
   </div>
 </template>
 
 <script>
+import ImageModule from '@/modules/ImageModule.vue';
+
 export default {
   name: 'Card',
+  components: {
+    ImageModule,
+  }
 };
 </script>
---
+++ src/modules/ImageModule.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="img__container">
+    <picture class="img__outer">
+      <source srcset="https://via.placeholder.com/640x800/d9efff/?text=Mobile+(640x800px)"
+        media="(max-width: 767px)">
+        <img class="img" src="https://via.placeholder.com/1920x800/ffe9b2/?text=Desktop+(1920x800px)" />
+    </picture>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageModule',
+};
+</script>
+
+<style lang="scss">
+@import '@/static/css/base/_mixins.scss';
+
+.img__container {
+  height: 0;
+  padding-top: calc(800 / 640 * 100%);
+  position: relative;
+  overflow: hidden;
+  background: gainsboro;
+
+  @include tablet {
+    padding-top: calc(800 / 1920 * 100%);
+  }
+
+  .img__outer {
+    .img {
+      display: block;
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  }
+}
+
+</style>
