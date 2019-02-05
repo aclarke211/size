a900d4913c9939d47e16c1a4446343d8da083e03
Alex Clarke <Alex.Clarke@jdplc.com>
2019-02-05 17:00:15 +0000

    Change between variants by having an array in 'data' that contains the current image for each of the products. When we want to change variants, update the entire array with new data including the images for the new variant. We need to set the current variant to the first one in the content when the page loads, this is done in update() as the products computed property has not been calculater when 'mounted()' runs or we would have run the code in 'updated()' in 'mounted()'. Doing it this way means we have to have a boolean to make sure the initial setup code only runs once.

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -2,10 +2,16 @@
   <div class="products__container" v-if="this.content[this.$route.params.year]">
     <h1 class="products-page__title">{{ $route.params.year }}</h1>
     <h2 class="products__subtitle">Products</h2>
+
+    <div v-for="(product, key) in products" :key="key">
     <Card
-      v-for="(product, key) in products" :key="key"
-      :image="product.variants[0].images.main.src" :text="product.name"
-    />
+      :image="productMainImages[key]" :text="product.name">
+      </Card>
+      <div v-for="(variant, key2) in product.variants" :key="key2"
+      @click="changeImage(key, key2)"
+      >{{variant.hexForIcon}}</div>
+    </div>
+
   </div>
 </template>
 
@@ -17,6 +23,10 @@ export default {
   components: {
     Card,
   },
+  data: () => ({
+    pageLoaded: false,
+    productMainImages: [],
+  }),
   props: {
     content: {
       type: [Object, Array],
@@ -27,6 +37,28 @@ export default {
     products() {
       return this.content[this.$route.params.year].products;
     },
+  },
+  methods: {
+    changeImage(productKey, variantKey) {
+      this.productMainImages = this.createNewProductImagesArray(productKey, variantKey);
+    },
+    createNewProductImagesArray(productKey, variantKey) {
+      const tempArray = [];
+      this.products.forEach((product, index) => {
+        if (productKey === index) {
+          tempArray.push(product.variants[variantKey].images.main.src);
+        } else {
+          tempArray.push(product.variants[0].images.main.src);
+        }
+      });
+      return tempArray;
+    },
+  },
+  updated() {
+    if (!this.pageLoaded) {
+      this.productMainImages = this.createNewProductImagesArray(0, 0);
+      this.pageLoaded = true;
+    }
   },
 };
 </script>
