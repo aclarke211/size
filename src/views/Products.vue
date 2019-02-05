6ed81c9715d68b4d3bcedc7e7b1c75c627ca680e
Alex Clarke <Alex.Clarke@jdplc.com>
2019-02-05 17:20:55 +0000

    Show swatches underneath each product (+ styling for swatches).

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -4,12 +4,15 @@
     <h2 class="products__subtitle">Products</h2>
 
     <div v-for="(product, key) in products" :key="key">
-    <Card
-      :image="productMainImages[key]" :text="product.name">
+      <Card
+        :image="productMainImages[key]" :text="product.name">
       </Card>
-      <div v-for="(variant, key2) in product.variants" :key="key2"
-      @click="changeImage(key, key2)"
-      >{{variant.hexForIcon}}</div>
+
+      <div class="swatches__container">
+        <div v-for="(variant, key2) in product.variants" :key="key2"
+        @click="changeImage(key, key2)" :style="hexColorStyling(key, key2)" class="swatch"
+        >{{variant.hexForIcon}}</div>
+      </div>
     </div>
 
   </div>
@@ -53,6 +56,11 @@ export default {
       });
       return tempArray;
     },
+    hexColorStyling(productKey, variantKey) {
+      const styles = {};
+      styles.backgroundColor = this.products[productKey].variants[variantKey].hexForIcon;
+      return styles;
+    },
   },
   updated() {
     if (!this.pageLoaded) {
@@ -62,3 +70,27 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+@import '@/static/css/base/_mixins.scss';
+
+.swatches__container {
+  @include flex;
+
+  .swatch {
+    @include flex;
+    border-radius: 100%;
+    border: 2px solid darkslategrey;
+    margin: 1rem;
+    width: 4rem;
+    height: 4rem;
+    font-size: .6rem;
+    opacity: .85;
+    transition: all .5s;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+}
+</style>
